793fb50f9a61e204bdd195c7677f4cb3837542f2
baodenrungxanh <baodenrungxanh@gmail.com>
2020-03-08 16:41:23 +0700

    Add files via upload

--- desktop.js
+++ desktop.js
@@ -1,57 +1,53 @@
 ﻿var thumbnailWidth = 155;
-alert(123)
 
-window.onerror = function () {
-    alert("Error caught");
-};
 
-function displayAlbum(msg, url, line, col, error) {
+window.onerror = function (msg, url, line, col, error) {
     alert(msg)
     alert(url)
     alert(line)
     alert(col)
     alert(error)
+};
+function displayAlbum(id) {
+
+    var albumString = $(`#news-${id} input[type='hidden'] `).val();
+    albumString = albumString.replace(/'/g, '"');
+    var albumItems = JSON.parse(albumString);
+
+    var imgs = $(`#news-${id} div[class='story__thumb'] img`);
+    for (var i = 0; i <= imgs.length - 1; i++) {
+        var imgSrc = albumItems[i].replace("/w700", `/w${thumbnailWidth}`);
+        imgs[i].setAttribute('src', imgSrc);
+    }
 }
 
 function displayVideo(id) {
-    try {
-        var videoData = $(`#news-${id} input[type='hidden']`).val();
-        videoData = videoData.replace(/'/g, '"');
-        videoData = JSON.parse(videoData);
-        $(`#news-${id} video `).attr('src', videoData.source);
-        $(`#news-${id} video `).attr('poster', videoData.poster);
-        $(`#news-${id} video `).attr('height', $("#post-list").width() * videoData.height / videoData.width);
+    var videoData = $(`#news-${id} input[type='hidden']`).val();
+    videoData = videoData.replace(/'/g, '"');
+    videoData = JSON.parse(videoData);
+    $(`#news-${id} video `).attr('src', videoData.source);
+    $(`#news-${id} video `).attr('poster', videoData.poster);
+    $(`#news-${id} video `).attr('height', $("#post-list").width() * videoData.height / videoData.width);
 
-        $("video:not([parsed='true'])").each(function () {
-            this.onloadedmetadata = function (e) {
-                var height = $(this).height();
+    $("video:not([parsed='true'])").each(function () {
+        this.onloadedmetadata = function (e) {
+            var height = $(this).height();
 
-               
+            var os = new OnScreen({
+                tolerance: height / 2,
+                toggleClass: false
+            });
 
-                try {
-                    var os = new OnScreen({
-                        tolerance: height / 2,
-                        toggleClass: false
-                    });
-
-                  
-                } catch (ess) {
-                    alert(ess.message)
-                }
-            }
-        });
-    } catch (e) {
-        alert(e.message)
-    }
+            // Do something else when an element leaves
+            os.on('leave', 'video', (element, event) => {
+                element.pause();
+            });
+        }
+    });
 }
 
 function convert(id) {
-    try {
-        document.getElementById("post-date-" + id).innerHTML = (moment(document.querySelector("div[id='id-" + id + "'] time").getAttribute("datetime")).startOf('minute ').fromNow());
-
-    } catch (e) {
-alert(e)
-    }
+    document.getElementById("post-date-" + id).innerHTML = (moment(document.querySelector("div[id='id-" + id + "'] time").getAttribute("datetime")).startOf('minute ').fromNow());
 }
 
 
