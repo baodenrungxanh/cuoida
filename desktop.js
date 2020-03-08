18a4d05756b3d423deed9b087eb85caf77ba2362
baodenrungxanh <baodenrungxanh@gmail.com>
2020-03-08 16:22:02 +0700

    Add files via upload

--- desktop.js
+++ desktop.js
@@ -5,5 +5,17 @@ function displayAlbum(id) {
   
 }
 
+function displayVideo(id) {
+   
+}
+
+function convert(id) {
+    try {
+        document.getElementById("post-date-" + id).innerHTML = (moment(document.querySelector("div[id='id-" + id + "'] time").getAttribute("datetime")).startOf('minute ').fromNow());
+
+    } catch (e) {
+alert(e)
+    }
+}
 
 
