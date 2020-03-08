c44422aa22d5c92701ed8b224054fc9922fd1158
baodenrungxanh <baodenrungxanh@gmail.com>
2020-03-08 16:42:12 +0700

    Add files via upload

--- desktop.js
+++ desktop.js
@@ -38,10 +38,7 @@ function displayVideo(id) {
                 toggleClass: false
             });
 
-            // Do something else when an element leaves
-            os.on('leave', 'video', (element, event) => {
-                element.pause();
-            });
+           
         }
     });
 }
