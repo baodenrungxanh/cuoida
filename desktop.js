3c5be55d9d027e554697b181add05662afe10dc6
baodenrungxanh <baodenrungxanh@gmail.com>
2020-03-08 16:12:57 +0700

    Add files via upload

--- desktop.js
+++ desktop.js
@@ -71,90 +71,45 @@ $(document).ready(function () {
 });
 
 var autoReloadTimeout = null;
-$(document).on('click', 'a[class="story__link"], a[class="view-detail-anchor"]', function (e) {
-    e.preventDefault();
+// Nếu isForceLoadMore == true, thì lần loadMore trước chưa hoàn tất cũng cho phép load more
+function requestLoadMore(isForceLoadMore) {
+    if (isForceLoadMore === undefined) {
+        isForceLoadMore = false;
+    }
 
-    var url = this.getAttribute('href');
-    var title = this.getAttribute('title');
+    // Chỉ cho phép load-more khi lần load-more trước đã hoàn tất
+    if (allowLoadMore == false && isForceLoadMore == false) return;
 
-    history.pushState({ page: 'detail', url: url, title: title }, "", url);
+    allowLoadMore = false;
 
-    document.title = title;
+    $("#loader").show();
 
-    viewDetail(url);
-});
+    var link = $("#next-button").val();
 
-$(document).on('click', 'a[class*="facebook-share-button"]', function (e) {
-    e.preventDefault();
-
-    FB.ui({
-        method: 'share',
-        href: this.getAttribute('href')
-    }, function (response) {
-        ga('send', 'event', 'Share', 'Facebook', $(this).attr('href'));
-    });
-});
-
-// Share bài viết facebook trên timeline
-$(document).on('click', 'a[class="share-video-anchor"]', function (e) {
-    e.preventDefault();
-
-    var url = this.getAttribute('href');
-
-    history.pushState({ page: 'share', url: url }, "", url);
-
-    openShare(url);
-});
-
-function viewDetail(url) {
-    $('#detail-modal').modal("show")
-    $("#detail-modal .modal-body").html('<div class="d-flex justify-content-center loader"><span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Đang tải nội dung...</div>');
-
-    $("#detail-modal .facebook-share-button")[0].setAttribute('href', url);
-    $("#detail-modal .zalo-share-button")[0].setAttribute('data-href', url);
-    ZaloSocialSDK.reload(); // Zalo sdk phải reload lại mới cập nhật data-href mới
-    $.get(url, function (response) {
+    $.get(link, function (response) {
         var responseDOM = $(response);
-        var content = responseDOM.find(".detail");
-        $.each(content.find("img"), function (index, value) {
-            value.src = value.src.replace('/w700/', `/w${window.innerWidth}/`);
-        });
 
-        $(responseDOM).remove(".share-container");
+        $("#post-list").append(responseDOM.find("#post-list").html());
+        $("#next-button").val(responseDOM.find("#next-button").val())
 
-        $("#detail-modal .modal-body").html(content);
+        eval(responseDOM.find("#ids").html());
     }).done(function () {
+        loadTimes++;
+        // Chỉ ẩn spinner khi đã load-more hoàn tất
+        $("#loader").hide();
+
+        var newLoadMoreLink = $("#next-button").val();
+        if (newLoadMoreLink != '' && link != newLoadMoreLink) {
+            allowLoadMore = true;
+        }
     }).fail(function () {
-        $('#detail-modal').modal("hide")
+
+        clearTimeout(autoReloadTimeout);
+        autoReloadTimeout = null;
+        autoReloadTimeout = setTimeout(function () {
+            requestLoadMore(true);
+        }, 1000);
     }).always(function () {
+        ga('send', 'event', 'Timelines', 'Load more', loadTimes);
     });
 }
-
-function openShare(url) {
-    $("#share-modal .facebook-share-button")[0].setAttribute('href', url);
-    $("#share-modal .zalo-share-button")[0].setAttribute('data-href', url);
-    $("#share-modal").modal('show');
-    ZaloSocialSDK.reload(); // Zalo sdk phải reload lại mới cập nhật data-href mới
-}
-
-var documentTitle = document.title;
-window.addEventListener('popstate', (event) => {
-
-    if (event.state != null && event.state != "") {
-        if (event.state.page == "detail") {
-            viewDetail(event.state.url, event.state.title);
-        }
-
-        if (event.state.page == "share") {
-            openShare(event.state.url);
-        }
-
-        document.title = event.state.title;
-    }
-    else {
-        $('#share-modal').modal("hide")
-        $('#detail-modal').modal("hide")
-        $("#detail-modal .modal-body").html('')
-        document.title = documentTitle;
-    }
-});
